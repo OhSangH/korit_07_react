6562852b079e9f6a283d2be9d3e740b1f39271dc
OhSangH <dhtkdgus126@gmail.com>
2025-10-20 17:46:44 +0900

    feat : 251020 react frontend RESTful API / Ag Grid / MUI

--- carfront/src/App.tsx
+++ carfront/src/App.tsx
@@ -1,9 +1,23 @@
-import './App.css';
+import { AppBar, Toolbar, Typography, Container, CssBaseline } from '@mui/material';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import CarList from './components/CarList';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
-      <div></div>
+      <Container maxWidth='xl'>
+        <CssBaseline />
+        <AppBar position='static'>
+          <Toolbar>
+            <Typography variant='h6'>Car Shop</Typography>
+          </Toolbar>
+        </AppBar>
+        <QueryClientProvider client={queryClient}>
+          <CarList />
+        </QueryClientProvider>
+      </Container>
     </>
   );
 }
